f1b88ee88b33b124e7dca39d23cfbc2e5877dd55
lucyygao <42822456+lucyygao@users.noreply.github.com>
2020-04-30 16:40:47 -0700

    paintwalls moved to building

--- socialdistancing/Building.java
+++ socialdistancing/Building.java
@@ -80,6 +80,31 @@ public class Building extends JPanel implements ActionListener{
 		control.paintPersons(g); // repaint all objects in simulation
 		
 	} 
+	
+	public void paintWalls(Graphics g) {
+
+		//draws vertical walls
+		g.drawImage(vWall1.getImage(), vWall1.getX(), vWall1.getY(), view);
+		g.drawImage(vWall2.getImage(), vWall2.getX(), vWall2.getY(), view);
+		g.drawImage(vWall3.getImage(), vWall3.getX(), vWall3.getY(), view);
+		g.drawImage(vWall4.getImage(), vWall4.getX(), vWall4.getY(), view);
+		
+		//draws horizontal walls
+		g.drawImage(hWall1.getImage(), hWall1.getX(), hWall1.getY(), view);
+		g.drawImage(hWall2.getImage(), hWall2.getX(), hWall2.getY(), view);
+		g.drawImage(hWall3.getImage(), hWall3.getX(), hWall3.getY(), view);
+		g.drawImage(hWall4.getImage(), hWall4.getX(), hWall4.getY(), view);
+		
+		//sets text color
+		g.setColor(Color.BLACK);
+		g.setFont(new Font("Roboto", Font.BOLD, 20));
+		
+		g.drawString("Sprouts", 610, 50);
+		g.drawString("Scripps Medical", 5, 50);
+		g.drawString("Board and Brew", 5, 440);
+		g.drawString("Mr. M's House", 590, 440);
+		
+	}
 		
 	
 }
